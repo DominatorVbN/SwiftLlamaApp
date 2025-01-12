b580ce10f9c6d49b8f1f8585094ea2d2031971ea
DominatorVbN <voilentbynature@gmail.com>
2025-01-12 18:57:24 +0800

    Initial Commit

---
+++ SwiftLlamaApp/SwiftLlamaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLlamaAppApp.swift
+//  SwiftLlamaApp
+//
+//  Created by Amit Samant on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLlamaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftLlamaAppTests/SwiftLlamaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLlamaAppTests.swift
+//  SwiftLlamaAppTests
+//
+//  Created by Amit Samant on 12/01/25.
+//
+
+import Testing
+@testable import SwiftLlamaApp
+
+struct SwiftLlamaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
